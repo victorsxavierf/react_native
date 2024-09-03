fe95e68b33f5e6c5577cb79bac8eff090ae9cc2c
vSxF7 <victorrxavierr7@gmail.com>
2024-09-02 21:56:53 -0300

    Estilização de Elemtentos

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
-import {Text} from "react-native"
+import {Text, View} from "react-native"
 
 export default function App(){
  return(
-  <Text> Teste de APP </Text>
+    <View style = {{
+        backgroundColor: '#151515'
+    }}>
+        <Text style = {{
+            textAlign: "center",
+            color: '#FFFFFF',
+            fontSize: 26
+        }}> Teste de APP </Text>
+    </View>
  )
 }
