871b9f8c23e4f60ba41ae4a0515c72704a286676
vSxF7 <victorrxavierr7@gmail.com>
2024-09-02 22:23:25 -0300

    Utilização de StyleSheet

--- App.tsx
+++ App.tsx
@@ -1,15 +1,30 @@
-import {Text, View} from "react-native"
+import {Text, View, StyleSheet} from "react-native"
 
 export default function App(){
  return(
-    <View style = {{
-        backgroundColor: '#151515'
-    }}>
-        <Text style = {{
-            textAlign: "center",
-            color: '#FFFFFF',
-            fontSize: 26
-        }}> Teste de APP </Text>
+    <View style = {styles.container}>
+        <Text style = {styles.eventName}> Teste de APP </Text>
+        <Text style = {styles.eventDate}> React Native </Text>
     </View>
  )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#151515'
+    },
+
+    eventName: {
+        marginTop: 50,
+        color: '#FFFFFF',
+        fontSize: 26,
+        textAlign: "center"
+    },
+
+    eventDate:{
+        color: '#195820',
+        fontSize: 26,
+        textAlign: "center"
+    }
+});
